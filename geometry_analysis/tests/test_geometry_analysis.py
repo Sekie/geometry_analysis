00c03a7817eb42985767cd339609f43108f4f12a
Henry Tran <big_hen@live.com>
2019-07-25 14:08:34 -0500

    add tests for packages

--- geometry_analysis/tests/test_geometry_analysis.py
+++ geometry_analysis/tests/test_geometry_analysis.py
@@ -6,7 +6,78 @@ Unit and regression test for the geometry_analysis package.
 import geometry_analysis
 import pytest
 import sys
+import numpy as np
+
+@pytest.fixture()
+def water_molecule():
+    name = "water"
+    symbol = ["H", "O", "H"]
+    coordinates = np.array([[2, 0, 0], [0, 0, 0], [-2, 0, 0]])
+
+    water = geometry_analysis.Molecule(name, symbol, coordinates)
+
+    return water
+
+def test_create_failure(): # Test for failure. Want this test to succeed if a failure is thrown.
+    name = 25
+    symbols = ["H", "O", "H"]
+    coordinates = np.zeros([3, 3])
+
+    with pytest.raises(TypeError):
+        water = geometry_analysis.Molecule(name, symbols, coordinates)
+
+def test_molecule_set_coordinates(water_molecule): # pass fixture as an argument to test function.
+    """Test that bond list is rebuilt when we reset coordinates"""
+    num_bonds = len(water_molecule.bonds)
+    assert num_bonds == 2
+    
+    new_coordinates = np.array([[5, 0, 0], [0, 0, 0], [-2, 0, 0]])
+    water_molecule.coordinates = new_coordinates
+    new_bonds = len(water_molecule.bonds)
+    assert new_bonds == 1 
+
+    assert np.array_equal(new_coordinates, water_molecule.coordinates)
 
 def test_geometry_analysis_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "geometry_analysis" in sys.modules
+
+def test_calculate_distance():
+    """Test the calculate_distance function"""
+    r1 = np.array([0, 0, -1])
+    r2 = np.array([0, 1, 0])
+    
+    expected_distance = np.sqrt(2.)
+
+    calculated_distance = geometry_analysis.calculate_distance(r1, r2)
+
+    assert expected_distance == calculated_distance
+
+def test_calc_angle_90():
+    """Test the calculate_angle function"""
+    r1 = np.array([0, 1, 0])
+    r2 = np.array([0, 0, 0])
+    r3 = np.array([0, 0, -1])
+
+    expected_angle = np.pi / 2.0
+    calculated_angle = geometry_analysis.calculate_angle(r1, r2, r3)
+    assert abs(expected_angle - calculated_angle) < 1e-10
+
+def test_calc_angle_60():
+    """Test another value of calculate_angle function"""
+    r1 = np.array([0, 0, -1])
+    r2 = np.array([0, 1, 0])
+    r3 = np.array([1, 0, 0])
+
+    expected_value = 60.
+
+    calculated_value = geometry_analysis.calculate_angle(r1, r2, r3, degrees = True)
+    assert np.isclose(expected_value, calculated_value) # How to compare floats.
+
+@pytest.mark.parametrize("p1, p2, p3, expected_angle", [
+    (np.array([1, 0, 0]), np.array([0, 0, 0]), np.array([0, 1, 0]), 90),
+    (np.array([0, 0, -1]), np.array([0, 1, 0]), np.array([1, 0, 0]), 60),
+]) # Decorates the function immediately below it.
+def test_calculate_angle(p1, p2, p3, expected_angle):
+    calculated_angle = geometry_analysis.calculate_angle(p1, p2, p3, degrees = True)
+    assert np.isclose(calculated_angle, expected_angle)
